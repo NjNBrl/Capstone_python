daa9121c24e8bc95edad0f670f8eef1766ad693a
NjNBrl <162153636+NjNBrl@users.noreply.github.com>
2024-07-03 21:46:44 +0545

    emailcheck.py

--- Packages/emailcheck.py
+++ Packages/emailcheck.py
@@ -1,5 +1,13 @@
 
 def email_check(email):
+      """
+    A subroutine that accepts email address and checks wether it is a valid email address containing a domain name
+    Args:
+        email (str): an email entered by the user
+
+    Returns:
+        bool: True or False after evaluation
+    """
     at = 0
     dot = 0
     if (email[0] == "." or email[len(email)-1] == ".") or (email[0] == "@" or email[len(email)-1] == "@") or (email[0] == "!" or email[len(email)-1] == "!") or (email[0] == "@" or email[len(email)-1] == "@"):
