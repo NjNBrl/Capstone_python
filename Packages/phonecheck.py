89c11abb8183dea539b3ece2280ecbc59d875dd8
NjNBrl <162153636+NjNBrl@users.noreply.github.com>
2024-07-03 21:46:06 +0545

    phonecheck.py

--- Packages/phonecheck.py
+++ Packages/phonecheck.py
@@ -1,4 +1,13 @@
 def check_phone(phone):
+    """
+    A subroutine that checks if the entered phone number is 10 digit or not
+
+    Args:
+        phone (int): Integer entered by the user
+
+    Returns:
+        bool: Evaluates and returns True or False
+    """
     str_phone = str(phone)
     if len(str_phone) == 10:
         return True
