b2475e10c97ca0833b8473ff60babf94d51715db
NjNBrl <162153636+NjNBrl@users.noreply.github.com>
2024-07-03 21:45:15 +0545

    markscheck.py

--- Packages/markscheck.py
+++ Packages/markscheck.py
@@ -1,4 +1,13 @@
 def marks_check(marks):
+    """
+    A subroutine that checks if the entered marks is in range between 0 and 100
+
+    Args:
+        marks (float): Marks of the student entered by the teacher
+
+    Returns:
+        bool: Evaluates and returns True or False
+    """
     if marks >= 0.0 and marks <= 100.0:
         return True
     else:
